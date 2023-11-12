0046bd44f5afc6235a2c77dc2c1267b017031908
subject9x <pwroohr@gmail.com>
2023-11-11 20:19:04 -0500

    feature: core rule pdfs served via get requests.

--- src/main/java/com/orbitalyards/landwar/mvc/controller/RulesController.java
+++ src/main/java/com/orbitalyards/landwar/mvc/controller/RulesController.java
@@ -1,18 +1,54 @@
 package com.orbitalyards.landwar.mvc.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin("*")
+/***
+ * Defines request routes for returning all rules PDF as their existing html files.
+ * 
+ * I am using thymeleaf views so that I can leverage link/href resolving via thymeleaf and its the fastest way
+ * to simply print raw html to pdf. Script segment at the bottom shows this.
+ * @author roohr
+ */
 public class RulesController {
 
-	@GetMapping(path = "/pdf/rules/core", produces = "application/pdf")
-	public ResponseEntity<String> getRulesCore() throws Exception{
+	@Value( "${landwar.app.version}" )
+	private String appVersion;
+	
+	@GetMapping(path = "/rules/pdf/core", produces = "application/html")
+	public String getRulesCore(Model model) throws Exception{
 		
+		model.addAttribute("version", "LANDWAR-Net v" + appVersion);
 		
-		return new ResponseEntity<String>("foob", HttpStatus.OK);
+		return "/fragments/pages/rules/rulebook_core";
 	}
 	
+	@GetMapping(path = "/rules/pdf/quick", produces = "application/html")
+	public String getRulesQuickplay(Model model) throws Exception{
+		
+		model.addAttribute("version", "LANDWAR-Net v" + appVersion);
+		
+		return "/fragments/pages/rules/rulebook_quickplay";
+	}
+	
+	@GetMapping(path = "/rules/pdf/scenario", produces = "application/html")
+	public String getRulesScenario(Model model) throws Exception{
+		
+		model.addAttribute("version", "LANDWAR-Net v" + appVersion);
+		
+		return "/fragments/pages/rules/rulebook_scenarios_basic";
+	}
+	
+	@GetMapping(path = "/rules/pdf/unitBuild", produces = "application/html")
+	public String getUnitBuildRules(Model model) throws Exception{
+		
+		model.addAttribute("version", "LANDWAR-Net v" + appVersion);
+		
+		return "/fragments/pages/rules/rulebook_core_unit_cost";
+	}
 }
